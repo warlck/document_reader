f94615adf593c823057d8e5f0c8a1c48365b55da
warlck <addick@me.com>
2014-01-14 16:29:29 +0800

    Solve minor issue with upload file spec, add support for filtering out the slow specs and add  support for deferred garbage collection

--- spec/features/upload_document_file_spec.rb
+++ spec/features/upload_document_file_spec.rb
@@ -20,12 +20,12 @@ feature 'User creating document by uploading pdf' do
 	 	log_in user
 	 	visit new_document_path 
 	 	expect(page).to have_content "Upload Document"
-	 	attach_file "File", "#{Rails.root}/public/unix-programming.pdf"
+	 	attach_file "document_uploaded_file", "#{Rails.root}/public/unix-programming.pdf"
 	 	click_button "Upload"
 	 	expect(current_path).to eq library_path user.library
 	 	expect(page).to have_css '.document'
 	 	expect(page).to have_content "unix-programming"
-	 	click_link 'Download'
+	 	click_link 'unix-programming.pdf'
 	 	expect(page.response_headers['Content-Type']).to eq 'application/pdf'
 	 end
 
